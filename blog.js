1e0962b25f99868c3f7a6b2a2a800e2f83fac261
Helliott Karraro <hkblcx@gmail.com>
2023-11-29 19:45:08 -0300

    upload das páginas blog, bootcamps e planos

---
+++ blog.js
@@ -0,0 +1,102 @@
+function cliqueArtigo1() {
+    var list = document.getElementById("list1");
+    var text = document.getElementById("task_name1").value;
+  
+    if(text.length === 0) {
+      alert("Não há nenhum caractere no seu comentário, digite algo.");
+      return;
+    }
+  
+    var listItem = document.createElement("li");
+    listItem.className = "list-item"
+  
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+  }
+  
+function cliqueArtigo2() {
+    var list = document.getElementById("list2");
+    var text = document.getElementById("task_name2").value;
+
+    if(text.length === 0) {
+        alert("Não há nenhum caractere no seu comentário, digite algo.");
+        return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-item"
+
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+}
+
+function cliqueArtigo3() {
+    var list = document.getElementById("list3");
+    var text = document.getElementById("task_name3").value;
+
+    if(text.length === 0) {
+        alert("Não há nenhum caractere no seu comentário, digite algo.");
+        return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-item"
+
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+}
+
+function cliqueArtigo4() {
+    var list = document.getElementById("list4");
+    var text = document.getElementById("task_name4").value;
+
+    if(text.length === 0) {
+        alert("Não há nenhum caractere no seu comentário, digite algo.");
+        return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-item"
+
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+}
+
+function cliqueArtigo5() {
+    var list = document.getElementById("list5");
+    var text = document.getElementById("task_name5").value;
+
+    if(text.length === 0) {
+        alert("Não há nenhum caractere no seu comentário, digite algo.");
+        return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-item"
+
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+}
+
+function cliqueArtigo6() {
+    var list = document.getElementById("list6");
+    var text = document.getElementById("task_name6").value;
+
+    if(text.length === 0) {
+        alert("Não há nenhum caractere no seu comentário, digite algo.");
+        return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-item"
+
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+}
+
---
+++ index.js
@@ -0,0 +1,79 @@
+// Lǵica
+const buttonLogica = document.querySelector(".logica")
+const modalLogica = document.querySelector(".menu-logica")
+const buttonCloseLogica = document.querySelector(".fecha-logica")
+
+buttonLogica.onclick = function () {
+    modalLogica.show() 
+}
+
+buttonCloseLogica.onclick = function () {
+    modalLogica.close()
+}
+
+// Python
+const buttonPython = document.querySelector(".python")
+const modalPython = document.querySelector(".menu-python")
+const buttonClosePython = document.querySelector(".fecha-python")
+
+buttonPython.onclick = function () {
+    modalPython.show() 
+}
+
+buttonClosePython.onclick = function () {
+    modalPython.close()
+}
+
+// SQL
+const buttonSql = document.querySelector(".sql")
+const modalSql = document.querySelector(".menu-sql")
+const buttonCloseSql = document.querySelector(".fecha-sql")
+
+buttonSql.onclick = function () {
+    modalSql.show() 
+}
+
+buttonCloseSql.onclick = function () {
+    modalSql.close()
+}
+
+// Html
+const buttonHtml = document.querySelector(".html")
+const modalHtml = document.querySelector(".menu-html")
+const buttonCloseHtml = document.querySelector(".fecha-html")
+
+buttonHtml.onclick = function () {
+    modalHtml.show() 
+}
+
+buttonCloseHtml.onclick = function () {
+    modalHtml.close()
+}
+
+// Css
+
+const buttonCss = document.querySelector(".css")
+const modalCss = document.querySelector(".menu-css")
+const buttonCloseCss = document.querySelector(".fecha-css")
+
+buttonCss.onclick = function () {
+    modalCss.show() 
+}
+
+buttonCloseCss.onclick = function () {
+    modalCss.close()
+}
+
+// Javascript
+const buttonJs = document.querySelector(".js")
+const modalJs = document.querySelector(".menu-js")
+const buttonCloseJs = document.querySelector(".fecha-js")
+
+buttonJs.onclick = function () {
+    modalJs.show() 
+}
+
+buttonCloseJS.onclick = function () {
+    modalJs.close()
+}
+
